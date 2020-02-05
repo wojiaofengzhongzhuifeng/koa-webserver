e7c212dc1e0477bb2dace7880fad9b83b66a0587
饶家俊 <raojiajun@bimwinner.com>
2020-02-06 07:41:20 +0800

    第一个路由

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const koa = require('koa');
+const Router = require('koa-router');
+
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  // ctx.router available
+  console.log(1);
+});
 
 let app = new koa();
+
+app.use(router.routes());
+
 app.listen(3000);
