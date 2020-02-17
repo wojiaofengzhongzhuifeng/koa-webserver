8de86e9664d3c66fb511032cda966deaed0b99c0
饶家俊 <raojiajun@bimwinner.com>
2020-02-17 22:20:42 +0800

    获取请求参数

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,10 +1,14 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/v1/book/list', (ctx, next) => {
+router.post('/:id/v1/book/list', (ctx, next) => {
+  console.log('请求参数 ctx.request.query', ctx.query.id);
+  console.log('路径参数 ctx.params', ctx.params);
+  console.log('post body 参数', ctx.request.body);
+  console.log('header 参数', ctx.request.headers);
   ctx.body = {
-    data: "v1/book"
-  }
+    data: 'v1/book'
+  };
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const koa = require('koa');
 const initCore = require('./core/init');
+const bodyParser = require('koa-bodyparser');
+
 
 let app = new koa();
+app.use(bodyParser());
 initCore(app);
 
-app.listen( 3000);
+app.listen(3000);
