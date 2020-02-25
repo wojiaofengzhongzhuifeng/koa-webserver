68a8b23fe505038ee4dca3d25a4d2c5cdc920411
饶家俊 <raojiajun@bimwinner.com>
2020-02-25 17:44:59 +0800

    5-9-1: 使用 prefix,完成接口

---
+++ app/api/v1/user.js
@@ -0,0 +1,17 @@
+const Router = require('koa-router');
+const router = new Router({
+  prefix: '/v1/user'
+});
+const {RegisterValidator} = require('../../../app/validators/validator');
+
+router.post('/register', async (ctx, next) => {
+  // 校验参数
+  const v = await new RegisterValidator().validate(ctx);
+  console.log('所有请求用户数据', v);
+
+  ctx.body = {
+    data: 'success get data'
+  };
+});
+
+module.exports = router;
