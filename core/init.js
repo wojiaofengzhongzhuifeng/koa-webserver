1d5bb521084d2cc625bc28ca0efd72d6cab0fa4b
饶家俊 <raojiajun@bimwinner.com>
2020-02-25 11:02:03 +0800

    5-3: 通过配置文件区分环境,将配置文件导入到 global 中

--- core/init.js
+++ core/init.js
@@ -9,12 +9,16 @@ function initLoadRouters(app){
   const routerPath = path.join(process.cwd(), './app/api');
   // bug: 未兼容 module.exports = {router] 情况
   requireDirectory(module, routerPath, {
-    visit: (obj)=>{
-      if(obj instanceof Router){
+    visit: (obj) => {
+      if (obj instanceof Router) {
         app.use(obj.routes());
       }
     }
   });
+
+  // 将 config/config.js 的对象配置到 global 中
+  const configPath = path.join(process.cwd(), './config/config.js');
+  global.config = require(configPath);
 }
 
 module.exports = initCore;
