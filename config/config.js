663fce47e8a79ac6a77edfec8afca1ca412ffff7
饶家俊 <raojiajun@bimwinner.com>
2020-02-25 16:52:32 +0800

    5-6: 创建配置对象, 用于连接服务器数据库

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // prod / dev
   env: 'prod',
+  dbData: {
+    host: 'localhost',
+    port: '3306',
+    userName: 'root',
+    password: 'sj15702097950',
+    dbName: 'test123321',
+  }
 };
